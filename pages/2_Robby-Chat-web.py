c98e698d76915f4764b2a292279d9f30c08e6226
ly2xxx <ly2xxx@hotmail.com>
2024-02-27 23:06:02 +0000

    tidy up modules and methods

--- pages/2_Robby-Chat-web.py
+++ pages/2_Robby-Chat-web.py
@@ -1,9 +1,6 @@
-import os
 import hashlib
 import streamlit as st
 from io import StringIO
-import re
-import sys
 from modules.history import ChatHistory
 from modules.layout import Layout
 from modules.utils import Utilities
@@ -94,48 +91,8 @@ if True:
                 chatbot = utils.setup_chatbot(
                     uploaded_file, st.session_state["model"], st.session_state["temperature"]
                 )
-                st.session_state["chatbot"] = chatbot
-                sidebar.download_model(st.session_state["vectordb"])
-
-                if st.session_state["ready"]:
-                    # Create containers for chat responses and user prompts
-                    response_container, prompt_container = st.container(), st.container()
-
-                    with prompt_container:
-                        # Display the prompt form
-                        is_ready, user_input = layout.prompt_form()
-
-                        # Initialize the chat history
-                        history.initialize(uploaded_file)
-
-                        # Reset the chat history if button clicked
-                        if st.session_state["reset_chat"]:
-                            history.reset(uploaded_file)
-
-                        if is_ready:
-                            # Update the chat history and display the chat messages
-                            history.append("user", user_input)
-
-                            old_stdout = sys.stdout
-                            sys.stdout = captured_output = StringIO()
-
-                            output = st.session_state["chatbot"].conversational_chat(user_input)
-
-                            sys.stdout = old_stdout
-
-                            history.append("assistant", output)
-
-                            # Clean up the agent's thoughts to remove unwanted characters
-                            thoughts = captured_output.getvalue()
-                            cleaned_thoughts = re.sub(r'\x1b\[[0-9;]*[a-zA-Z]', '', thoughts)
-                            cleaned_thoughts = re.sub(r'\[1m>', '', cleaned_thoughts)
-
-                            # Display the agent's thoughts
-                            with st.expander("Display the agent's thoughts"):
-                                st.write(cleaned_thoughts)
-
-                    sidebar.download_conversation(st.session_state["history"], uploaded_file.name)
-                    history.generate_messages(response_container)
+                
+                utils.setup_conversation_cockpit(layout, sidebar, history, uploaded_file, chatbot)
             except Exception as e:
                 st.error(f"Error: {str(e)}")
 
