9940a9f0b302fe49e2489becb0c77b61d0f7cac8
ly2xxx <ly2xxx@hotmail.com>
2024-11-01 00:47:44 +0000

    Change chatbot prompt to tap into LLM's own knowledge base

--- modules/chatbot.py
+++ modules/chatbot.py
@@ -23,10 +23,22 @@ class Chatbot:
         self.temperature = temperature
         self.vectors = vectors
 
+    # qa_template = """
+    #     You are a helpful AI assistant named Robby. The user gives you a file its content is represented by the following pieces of context, use them to answer the question at the end.
+    #     If you don't know the answer, just say you don't know. Do NOT try to make up an answer.
+    #     If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+    #     Use as much detail as possible when responding.
+
+    #     context: {context}
+    #     =========
+    #     question: {question}
+    #     ======
+    #     """
+    
     qa_template = """
         You are a helpful AI assistant named Robby. The user gives you a file its content is represented by the following pieces of context, use them to answer the question at the end.
         If you don't know the answer, just say you don't know. Do NOT try to make up an answer.
-        If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+        If the question is not related to the context, search in your own knowledge base. But if you do find the answer in your own knowledge base, politely tell the user the provided context is not relevant and you have found the answer from somewhere else. And be as specific about the source of the answer as possible.
         Use as much detail as possible when responding.
 
         context: {context}
